ac38a54b887d3c2491702ce50368729d0790607b
XD-WOLF <62610377+nirajxhrestha@users.noreply.github.com>
2020-07-27 21:30:16 +0545

    Add files via upload

---
+++ PUBGMobMacro control tool/PUBGMobMacro/Overlay.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace PUBGMobMacro
+{
+    partial class Overlay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGun = new System.Windows.Forms.Label();
+            this.lblisEnabled = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCrouch = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblGun
+            // 
+            this.lblGun.AutoSize = true;
+            this.lblGun.BackColor = System.Drawing.Color.Transparent;
+            this.lblGun.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGun.Location = new System.Drawing.Point(137, 383);
+            this.lblGun.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblGun.Name = "lblGun";
+            this.lblGun.Size = new System.Drawing.Size(30, 17);
+            this.lblGun.TabIndex = 0;
+            this.lblGun.Text = "null";
+            // 
+            // lblisEnabled
+            // 
+            this.lblisEnabled.AutoSize = true;
+            this.lblisEnabled.BackColor = System.Drawing.Color.Transparent;
+            this.lblisEnabled.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblisEnabled.Location = new System.Drawing.Point(137, 411);
+            this.lblisEnabled.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblisEnabled.Name = "lblisEnabled";
+            this.lblisEnabled.Size = new System.Drawing.Size(63, 17);
+            this.lblisEnabled.TabIndex = 1;
+            this.lblisEnabled.Text = "Disabled";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 383);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Currently selected:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(63, 439);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Crouching:";
+            // 
+            // lblCrouch
+            // 
+            this.lblCrouch.AutoSize = true;
+            this.lblCrouch.BackColor = System.Drawing.Color.Transparent;
+            this.lblCrouch.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCrouch.Location = new System.Drawing.Point(137, 439);
+            this.lblCrouch.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCrouch.Name = "lblCrouch";
+            this.lblCrouch.Size = new System.Drawing.Size(30, 17);
+            this.lblCrouch.TabIndex = 4;
+            this.lblCrouch.Text = "null";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 1007);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "by NiRAJ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(88, 411);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Macro:";
+            // 
+            // Overlay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblCrouch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblisEnabled);
+            this.Controls.Add(this.lblGun);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Overlay";
+            this.Text = "Overlay";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Overlay_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGun;
+        private System.Windows.Forms.Label lblisEnabled;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCrouch;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
